09b121d532e1305b136ac3b96474a64c1b19f71d
GitHubEnjoyer1122 <m.afdhalrezkikasyah@gmail.com>
2023-12-17 17:05:03 +0800

    Commit 2

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,8 +8,9 @@ use App\Models\Student;
 class StudentController extends Controller
 {
     public function getAllStudent(){
-        $Student = Student::all();
+        $Student = Student::where("tahun_angkatan", "=", strval(now()->year-1).'/'.strval(now()->year))->get();
         
+
         if($Student){
             return response()->json([
                 "Message" => "Data received!",
