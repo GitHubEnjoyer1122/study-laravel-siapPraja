e7e34717ed69e0afc555dc6844e61e7b830aa602
GitHubEnjoyer1122 <m.afdhalrezkikasyah@gmail.com>
2023-12-17 17:10:20 +0800

    Tes Commit Server Only

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,7 +8,7 @@ use App\Models\Student;
 class StudentController extends Controller
 {
     public function getAllStudent(){
-        $Student = Student::where("tahun_angkatan", "=", strval(now()->year-1).'/'.strval(now()->year))->get();
+        $Student = Student::where("tahun_angkatan", "=", strval(now()->year).'/'.strval(now()->year+1))->get();
         
 
         if($Student){
